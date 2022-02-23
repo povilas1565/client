287c7c4f6a4d978ab25ee03adff41a872835b3fe
paul156551 <pavel.ryzhkov.99@mail.ru>
2022-02-23 22:56:41 +0300

    Фронт-часть проекта

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../services/auth.service";
+import {TokenStorageService} from "../../services/token-storage.service";
+import {NotificationService} from "../../services/notification.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public loginForm!: FormGroup;
+  constructor(private authService: AuthService,
+              private tokenService: TokenStorageService,
+              private notificationService: NotificationService,
+              private router: Router,
+              private formBuilder: FormBuilder)
+  {
+    if (this.tokenService.getUser()) {
+      this.router.navigate(['/']);
+    }
   }
 
+  createLoginForm(): FormGroup {
+    return this.formBuilder.group({
+      username: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.compose([Validators.required])],
+    })
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this.createLoginForm();
+  }
+
+  submitDataToServer(): void {
+    this.authService.login({
+      username: this.loginForm.value.username,
+      password: this.loginForm.value.password
+    }).subscribe(data => {
+      console.log(data);
+
+      this.tokenService.saveToken(data.token);
+      this.tokenService.saveUser(data);
+
+      this.notificationService.showSnackBar('Successful authorization');
+      this.router.navigate(['/index']);
+      window.location.reload();
+    }
+      ,error => {
+      console.log(error);
+      this.notificationService.showSnackBar('Incorrect login or password' + error.message);
+    });
+  }
 }
