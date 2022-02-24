6afd0d53a3eb8adb9f2c429e914d53be9148c548
paul156551 <pavel.ryzhkov.99@mail.ru>
2022-02-24 23:23:12 +0300

    Фронт-часть проекта

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MaterialModule} from ".material/material.module";
+import {MaterialModule} from "./material-module";
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {authInterceptorProviders} from "./helper/auth-interceptor.service";
--- src/app/material-module.ts
+++ src/app/material-module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from "@angular/core";
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
@@ -10,19 +10,22 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatDividerModule} from '@angular/material/divider';
 
-@NgModule({
-    exports: [
-      MatIconModule,
-      MatCardModule,
-      MatButtonModule,
-      MatInputModule,
-      MatFormFieldModule,
-      MatMenuModule,
-      MatToolbarModule,
-      MatSnackBarModule,
-      MatDialogModule,
-      MatDividerModule
-    ]
-  })
 
-export class MaterialModule { }
+@NgModule({
+  exports: [
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    MatDividerModule
+  ]
+})
+
+export class MaterialModule {
+
+}
